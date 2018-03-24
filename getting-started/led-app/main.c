95b145f7f97a1b539fa3cbf48f837594faa4ff14
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-03-24 15:27:08 +0100

    add led application

---
+++ getting-started/led-app/main.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "led.h"
+#include "board.h"
+#include "shell.h"
+
+static int led_toggle_handler(int argc, char** argv)
+{
+    if (argc < 2) {
+        printf("%s <led num>\n", argv[0]);
+        return 1;
+    }
+
+    uint8_t led_num = atoi(argv[1]);
+    switch(led_num) {
+        case 0:
+            LED0_TOGGLE;
+            break;
+        case 1:
+            LED1_TOGGLE;
+            break;
+        case 2:
+            LED2_TOGGLE;
+            break;
+        case 3:
+            LED3_TOGGLE;
+            break;
+        default:
+            printf("Unsupported LED number '%d'\n", led_num);
+            return 1;
+    }
+
+    return 0;
+}
+
+static const shell_command_t shell_commands[] = {
+    { "toggle", "toggle a led status", led_toggle_handler },
+    { NULL, NULL, NULL }
+};
+
+int main(void)
+{
+    puts("LED toggle shell application");
+
+    char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+    return 0;
+}
