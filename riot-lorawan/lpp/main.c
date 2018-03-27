946820ad9d38963f067c48cba561098e22fa9d3a
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-03-27 16:48:39 +0200

    riot-lorawan/lpp

---
+++ riot-lorawan/lpp/main.c
@@ -0,0 +1,108 @@
+#include <string.h>
+
+#include "thread.h"
+
+#include "net/loramac.h"
+#include "semtech_loramac.h"
+
+#include "hts221.h"
+#include "hts221_params.h"
+
+#include "cayenne_lpp.h"
+
+#include "board.h"
+
+#define SENDER_PRIO         (THREAD_PRIORITY_MAIN - 1)
+static kernel_pid_t sender_pid;
+static char sender_stack[THREAD_STACKSIZE_MAIN / 2];
+
+/* Declare globally the loramac descriptor */
+static semtech_loramac_t loramac;
+
+/* Declare globally the sensor device descriptor */
+static hts221_t hts221;
+
+/* Declare globally Cayenne LPP descriptor */
+
+/* Device and application informations required for OTAA activation */
+static const uint8_t deveui[LORAMAC_DEVEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appeui[LORAMAC_APPEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+static void *sender(void *arg)
+{
+    (void)arg;
+
+    while (1) {
+        /* do some measurements */
+        uint16_t humidity = 0;
+        int16_t temperature = 0;
+        if (hts221_read_humidity(&hts221, &humidity) != HTS221_OK) {
+            puts(" -- failed to read humidity!");
+        }
+        if (hts221_read_temperature(&hts221, &temperature) != HTS221_OK) {
+            puts(" -- failed to read temperature!");
+        }
+
+        /* prepare cayenne lpp payload */
+
+        printf("Sending LPP data\n");
+
+        /* send the message every 20 seconds */
+        semtech_loramac_send(&loramac, lpp.buffer, lpp.cursor);
+        /* Wait until the send cycle has completed */
+        semtech_loramac_recv(&loramac);
+
+        /* clear buffer once done */
+
+        xtimer_sleep(20);
+    }
+
+    /* this should never be reached */
+    return NULL;
+}
+
+int main(void)
+{
+    if (hts221_init(&hts221, &hts221_params[0]) != HTS221_OK) {
+        puts("Sensor initialization failed");
+        LED3_TOGGLE;
+        return 1;
+    }
+    if (hts221_power_on(&hts221) != HTS221_OK) {
+        puts("Sensor initialization power on failed");
+        LED3_TOGGLE;
+        return 1;
+    }
+    if (hts221_set_rate(&hts221, hts221.p.rate) != HTS221_OK) {
+        puts("Sensor continuous mode setup failed");
+        LED3_TOGGLE;
+        return 1;
+    }
+
+    /* initialize the loramac stack */
+    semtech_loramac_init(&loramac);
+
+    /* use a fast datarate so we don't use the physical layer too much */
+    semtech_loramac_set_dr(&loramac, 5);
+
+    /* set the LoRaWAN keys */
+    semtech_loramac_set_deveui(&loramac, deveui);
+    semtech_loramac_set_appeui(&loramac, appeui);
+    semtech_loramac_set_appkey(&loramac, appkey);
+
+    /* start the OTAA join procedure */
+    puts("Starting join procedure");
+    if (semtech_loramac_join(&loramac, LORAMAC_JOIN_OTAA) != SEMTECH_LORAMAC_JOIN_SUCCEEDED) {
+        puts("Join procedure failed");
+        return 1;
+    }
+
+    puts("Join procedure succeeded");
+
+    /* start the sender thread */
+    sender_pid = thread_create(sender_stack, sizeof(sender_stack),
+                               SENDER_PRIO, 0, sender, NULL, "sender");
+
+    return 0;
+}
