ee5bec62858e251fdfe2b96fd7db8b1f48c0cadc
Alexandre Abadie <alexandre.abadie@inria.fr>
2018-03-27 16:37:04 +0200

    riot-lorawan/thread

---
+++ riot-lorawan/thread/main.c
@@ -0,0 +1,59 @@
+#include <string.h>
+
+#include "thread.h"
+#include "xtimer.h"
+
+#include "net/loramac.h"
+#include "semtech_loramac.h"
+
+#include "board.h"
+
+#define SENDER_PRIO         (THREAD_PRIORITY_MAIN - 1)
+static kernel_pid_t sender_pid;
+static char sender_stack[THREAD_STACKSIZE_MAIN / 2];
+
+/* Declare globally the loramac descriptor */
+static semtech_loramac_t loramac;
+
+/* Device and application informations required for OTAA activation */
+static const uint8_t deveui[LORAMAC_DEVEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appeui[LORAMAC_APPEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+/* The simple message to send */
+const char *message = "This is RIOT!";
+
+static void *sender(void *arg)
+{
+    (void)arg;
+
+    /* this should never be reached */
+    return NULL;
+}
+
+int main(void)
+{
+    /* initialize the loramac stack */
+    semtech_loramac_init(&loramac);
+
+    /* use a fast datarate so we don't use the physical layer too much */
+    semtech_loramac_set_dr(&loramac, 5);
+
+    /* set the LoRaWAN keys */
+    semtech_loramac_set_deveui(&loramac, deveui);
+    semtech_loramac_set_appeui(&loramac, appeui);
+    semtech_loramac_set_appkey(&loramac, appkey);
+
+    /* start the OTAA join procedure */
+    puts("Starting join procedure");
+    if (semtech_loramac_join(&loramac, LORAMAC_JOIN_OTAA) != SEMTECH_LORAMAC_JOIN_SUCCEEDED) {
+        puts("Join procedure failed");
+        return 1;
+    }
+
+    puts("Join procedure succeeded");
+
+    /* start the sender thread */
+
+    return 0; /* should never be reached */
+}
