42c92976081cbab81a9bea6d2a3d7049f493526b
Jose Alamos <jose.alamos@haw-hamburg.de>
2018-09-11 14:20:13 +0200

    add solutions under .solution folder

---
+++ riot-lorawan/downlink/.solution/main.c
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2018 Inria
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <string.h>
+
+#include "xtimer.h"
+
+#include "net/loramac.h"
+#include "semtech_loramac.h"
+
+#include "board.h"
+
+/* Declare globally the loramac descriptor */
+static semtech_loramac_t loramac;
+
+/* Device and application informations required for OTAA activation */
+static const uint8_t deveui[LORAMAC_DEVEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appeui[LORAMAC_APPEUI_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const uint8_t appkey[LORAMAC_APPKEY_LEN] = { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+
+/* The simple message to send */
+const char *message = "This is RIOT!";
+
+static void sender(void)
+{
+    while (1) {
+        printf("Sending message: %s\n", message);
+        /* send the message every 20 seconds */
+        semtech_loramac_send(&loramac, (uint8_t *)message, strlen(message));
+        /* Wait until the send cycle has completed */
+        if (semtech_loramac_recv(&loramac) == SEMTECH_LORAMAC_DATA_RECEIVED) {
+            loramac.rx_data.payload[loramac.rx_data.payload_len] = 0;
+            printf("Data received: %s\n", (char *)loramac.rx_data.payload);
+            uint8_t count = 10;
+            while (count--) {
+                LED1_TOGGLE;
+                xtimer_usleep(100 * US_PER_MS);
+                LED1_TOGGLE;
+                xtimer_usleep(100 * US_PER_MS);
+            }
+        }
+
+        xtimer_sleep(20);
+    }
+
+    /* this should never be reached */
+    return NULL;
+}
+
+int main(void)
+{
+    /* initialize the loramac stack */
+    semtech_loramac_init(&loramac);
+
+    /* use a fast datarate so we don't use the physical layer too much */
+    semtech_loramac_set_dr(&loramac, 5);
+
+    /* set the LoRaWAN keys */
+    semtech_loramac_set_deveui(&loramac, deveui);
+    semtech_loramac_set_appeui(&loramac, appeui);
+    semtech_loramac_set_appkey(&loramac, appkey);
+
+    /* start the OTAA join procedure */
+    puts("Starting join procedure");
+    if (semtech_loramac_join(&loramac, LORAMAC_JOIN_OTAA) != SEMTECH_LORAMAC_JOIN_SUCCEEDED) {
+        puts("Join procedure failed");
+        return 1;
+    }
+
+    puts("Join procedure succeeded");
+
+    /* call the sender */
+    sender();
+
+    return 0; /* should never be reached */
+}
